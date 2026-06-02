2045fd2c923ad3c5d7b84862e20f8d5b43497b69
Erik Schaareman <erik@schaareman.org>
2026-06-02 22:58:25 +0200

    chore: bootstrap homebrew tap

---
+++ Formula/vstack.rb
@@ -0,0 +1,25 @@
+class Vstack < Formula
+  include Language::Python::Virtualenv
+
+  desc "VS Code-native AI engineering workflow system"
+  homepage "https://github.com/eschaar/vstack"
+  url "https://files.pythonhosted.org/packages/e5/50/fe8c8220c952f7215b0cf97ea434af6c0d4eda372d95957da95a1539e7fc/vstack-3.4.2.tar.gz"
+  sha256 "42e54d8e390b18d7b9c80df314fe593c017efebe959c2a5f1a031ffc140e084a"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
+    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    output = shell_output("#{bin}/vstack --help")
+    assert_match "Usage:", output
+  end
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# homebrew-vstack
+
+Homebrew tap for vstack.
+
+## Install
+
+```bash
+brew tap eschaar/vstack
+brew install vstack
+```
+
+Alternative without prior tap:
+
+```bash
+brew install eschaar/vstack/vstack
+```
+
+## Maintenance
+
+This repository is updated automatically from the release pipeline in [eschaar/vstack](https://github.com/eschaar/vstack) through a signed `repository_dispatch` event.
